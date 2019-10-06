a7418a48f8fb2315ab1be78cfca14867a98a0c46
mkarousou <mkarousou@gmail.com>
2019-10-06 18:00:40 +0300

    initial commit, implemented model

---
+++ src/addressbook/Contact.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addressbook;
+
+/**
+ *
+ * @author gtsatiris
+ */
+public class Contact {
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    
+    public Contact (String name, String phone, String email, String address) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+    
+    public String getName (){
+        return this.name;
+    }  
+    
+    public String getPhone (){
+        return this.phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setName (String name){
+        this.name = name;  
+    }
+    
+    public void setPhone (String phone){
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+}
