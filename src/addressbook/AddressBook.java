24563e910250c77b4643844b4168e097031695e3
mkarousou <mkarousou@gmail.com>
2019-10-08 11:34:56 +0300

    implemented view main loop

--- src/addressbook/AddressBook.java
+++ src/addressbook/AddressBook.java
@@ -15,7 +15,8 @@ public class AddressBook {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ContactView view = new ContactView();
+        view.mainLoop();
     }
     
 }
--- src/addressbook/ContactView.java
+++ src/addressbook/ContactView.java
@@ -20,43 +20,44 @@ public class ContactView {
    }
    
    public void mainLoop(){
-       System.out.println("Menu");
-       System.out.println("1. View all contacts");
-       System.out.println("2. Add new contact");
-       System.out.println("3. Search contact by name");
-       System.out.println("4. Search contact by phone");
-       System.out.println("5. Modify contact by name");
-       System.out.println("6. Delete contact by name");
-       System.out.println("7. Exit");
-       
-       Scanner inScn = new Scanner(System.in);
-       String input = inScn.nextLine();
-       if(input.equals("1")){
-           
+        while(true) {
+            System.out.println("Menu");
+            System.out.println("1. View all contacts");
+            System.out.println("2. Add new contact");
+            System.out.println("3. Search contact by name");
+            System.out.println("4. Search contact by phone");
+            System.out.println("5. Modify contact by name");
+            System.out.println("6. Delete contact by name");
+            System.out.println("7. Exit");
+
+            Scanner inScn = new Scanner(System.in);
+            String input = inScn.nextLine();
+            if(input.equals("1")){
+                this.showContacts();
+            }
+            else if(input.equals("2")){
+                this.processCreation();
+            }
+            else if(input.equals("3")){
+                this.processSearchByName();
+            }
+            else if(input.equals("4")){
+                this.processSearchByPhone();
+            }
+            else if(input.equals("5")){
+                this.processModification();
+            }
+            else if(input.equals("6")){
+                this.processDelete();
+            }
+            else if(input.equals("7")){
+                this.processExit();
+                break;
+            }
+            else{
+                System.out.println("Wrong input!! Must be between 1 and 7!");
+            }
        }
-       else if(input.equals("2")){
-           
-       }
-       else if(input.equals("3")){
-           
-       }
-       else if(input.equals("4")){
-           
-       }
-       else if(input.equals("5")){
-           
-       }
-       else if(input.equals("6")){
-           
-       }
-       else if(input.equals("7")){
-           
-       }
-       else{
-           
-       }
-           
-       
    }
    
    public void showContacts(){
@@ -75,6 +76,9 @@ public class ContactView {
        
    }
    
+   public void processModification(){
+       
+   }
    public void processDelete(){
        
    }
